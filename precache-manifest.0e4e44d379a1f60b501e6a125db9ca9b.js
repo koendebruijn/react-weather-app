d4d6673fa6bdf0b3d319a39a9091ba1f50507a9f
koendebruijn <bruijnk@gmail.com>
2020-03-17 21:11:21 +0100

    Updates

---
+++ precache-manifest.0e4e44d379a1f60b501e6a125db9ca9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "406dce8e0c0efbfd8af63baf8f2e08fe",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "2a8077b8710a0c1f5114",
+    "url": "/react-weather-app/static/css/main.90d05288.chunk.css"
+  },
+  {
+    "revision": "61f9aa89fc513c31c075",
+    "url": "/react-weather-app/static/js/2.94f7ae3a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-weather-app/static/js/2.94f7ae3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a8077b8710a0c1f5114",
+    "url": "/react-weather-app/static/js/main.bfabe46c.chunk.js"
+  },
+  {
+    "revision": "68c1b77c065710be465a",
+    "url": "/react-weather-app/static/js/runtime-main.5b769f5e.js"
+  }
+]);
